df75506856fed593ff92b6d34500f22c00fe6928
uchitate <ry0suke.uch1tate@gmail.com>
2017-07-01 20:46:17 +0900

    モック作成

--- src/main/java/jp/co/waja/core/web/controller/admin/staff/StaffController.java
+++ src/main/java/jp/co/waja/core/web/controller/admin/staff/StaffController.java
@@ -2,6 +2,7 @@ package jp.co.waja.core.web.controller.admin.staff;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/staff")
@@ -11,5 +12,20 @@ public class StaffController {
 	@GetMapping("/list")
 	public String list() {
 		return "admin/staff/list";
+	}
+
+	@GetMapping("/create")
+	public String create() {
+		return "admin/staff/create";
+	}
+
+	@GetMapping("/describe/{staffId}")
+	public String describe(@PathVariable Long staffId) {
+		return "admin/staff/describe";
+	}
+
+	@GetMapping("/edit/{staffId}")
+	public String edit(@PathVariable Long staffId) {
+		return "admin/staff/edit";
 	}
 }
