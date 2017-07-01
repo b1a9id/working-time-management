79ee04338d38511118b0aca373d8acee2ac0ff62
uchitate <ry0suke.uch1tate@gmail.com>
2017-07-01 22:00:17 +0900

    モック作成

---
+++ src/main/java/jp/co/waja/core/web/controller/admin/team/TeamController.java
@@ -0,0 +1,31 @@
+package jp.co.waja.core.web.controller.admin.team;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/team")
+public class TeamController {
+
+	@GetMapping("/list")
+	public String list() {
+		return "admin/team/list";
+	}
+
+	@GetMapping("/create")
+	public String create() {
+		return "admin/team/create";
+	}
+
+	@GetMapping("/describe/{teamId}")
+	public String describe(@PathVariable Long teamId) {
+		return "admin/team/describe";
+	}
+
+	@GetMapping("/edit/{teamId}")
+	public String edit(@PathVariable Long teamId) {
+		return "admin/team/edit";
+	}
+}
