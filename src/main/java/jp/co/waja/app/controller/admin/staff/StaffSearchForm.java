9b734b17a4f3a9cb825486d9df68b6a678664a53
Ryosuke Uchitate <u.ryosuke@waja.jp>
2017-08-28 19:19:55 +0900

    ブラッシュアップ

--- src/main/java/jp/co/waja/app/controller/admin/staff/StaffSearchForm.java
+++ src/main/java/jp/co/waja/app/controller/admin/staff/StaffSearchForm.java
@@ -1,16 +1,24 @@
 package jp.co.waja.app.controller.admin.staff;
 
+import jp.co.waja.core.entity.WorkTime;
 import jp.co.waja.core.model.staff.StaffSearchRequest;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.io.Serializable;
+import java.time.LocalDate;
 
 @Getter
 @Setter
 public class StaffSearchForm implements Serializable {
 
 	private Long teamId;
+
+	private WorkTime.WorkType workType;
+
+	private LocalDate enteredDate;
+
+	private boolean telework;
 
 	public StaffSearchRequest toStaffSearchRequest() {
 		return new StaffSearchRequest(getTeamId());
