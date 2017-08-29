c9394a37e38b783d8645652a0ea601c79591879a
Ryosuke Uchitate <u.ryosuke@waja.jp>
2017-08-29 11:17:47 +0900

    入力完了報告実装

--- src/main/java/jp/co/waja/app/controller/user/worktime/WorkTimeCompleteController.java
+++ src/main/java/jp/co/waja/app/controller/user/worktime/WorkTimeCompleteController.java
@@ -3,11 +3,15 @@ package jp.co.waja.app.controller.user.worktime;
 import jp.co.waja.core.entity.WorkTimeYearMonth;
 import jp.co.waja.core.service.staff.StaffDetails;
 import jp.co.waja.core.service.worktime.WorkTimeService;
+import jp.co.waja.core.support.WorkTimeUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.time.YearMonth;
+import java.time.format.DateTimeFormatter;
 
 @Controller
 @RequestMapping("/work-time/complete")
@@ -22,7 +26,12 @@ public class WorkTimeCompleteController {
 			@PathVariable String displayYearMonth,
 			@RequestParam Boolean complete,
 			RedirectAttributes redirectAttributes) {
-		if (!complete) {
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM");
+		YearMonth parsedDisplayYearMonth = YearMonth.parse(displayYearMonth, formatter);
+		WorkTimeYearMonth workTimeYearMonth =
+				workTimeService.getWorkTimeYearMonth(loginUser.getStaff(), WorkTimeUtil.yearMonthToInt(parsedDisplayYearMonth));
+		if (workTimeYearMonth.isComplete()) {
+			redirectAttributes.addFlashAttribute("notModify", "notModify");
 			return "redirect:/work-time?error";
 		}
 		WorkTimeYearMonth updatedWorkTimeYearMonth =
