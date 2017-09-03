38969fd8890dd78fd8baab3e6fb25c1db008af8f
Ryosuke Uchitate <ry0suke.uch1tate@gmail.com>
2017-09-03 17:21:17 +0900

    時間が不必要なWorkTypeの時間を非表示にする

--- src/main/java/jp/co/waja/app/controller/user/worktime/WorkTimeYearMonthEditForm.java
+++ src/main/java/jp/co/waja/app/controller/user/worktime/WorkTimeYearMonthEditForm.java
@@ -1,5 +1,6 @@
 package jp.co.waja.app.controller.user.worktime;
 
+import jp.co.waja.core.entity.WorkTime;
 import jp.co.waja.core.entity.WorkTimeYearMonth;
 import jp.co.waja.core.model.worktime.WorkTimeEditRequest;
 import jp.co.waja.core.model.worktime.WorkTimeYearMonthEditRequest;
@@ -9,10 +10,12 @@ import lombok.Setter;
 
 import java.io.Serializable;
 import java.time.LocalTime;
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import static java.util.Objects.nonNull;
+import static jp.co.waja.core.entity.WorkTime.WorkType.*;
 
 @Getter
 @Setter
@@ -32,24 +35,33 @@ public class WorkTimeYearMonthEditForm implements Serializable {
 	public WorkTimeYearMonthEditRequest toWorkTimeYearMonthEditRequest() {
 		WorkTimeYearMonthEditRequest request = new WorkTimeYearMonthEditRequest();
 		List<WorkTimeEditRequest> editRequests = getWorkTimes().stream()
-				.map(workTime -> {
-					WorkTimeEditRequest editRequest = new WorkTimeEditRequest();
-					editRequest.setDate(workTime.getDate());
-					editRequest.setWorkType(workTime.getWorkType());
-					if (nonNull(workTime.getStartAtHour()) && nonNull(workTime.getStartAtMinute())) {
-						LocalTime startAt = LocalTime.of(workTime.getStartAtHour(), workTime.getEndAtMinute());
-						editRequest.setStartAt(startAt);
-					}
-					if (nonNull(workTime.getEndAtHour()) && nonNull(workTime.getEndAtMinute())) {
-						LocalTime endAt = LocalTime.of(workTime.getEndAtHour(), workTime.getEndAtMinute());
-						editRequest.setEndAt(endAt);
-					}
-					editRequest.setRestTime(workTime.getRestTime());
-					editRequest.setRemarks(workTime.getRemarks());
-					return editRequest;
-				}).collect(Collectors.toList());
+				.map(this::toWorkTimeEditRequest)
+				.collect(Collectors.toList());
 		request.setId(getId());
 		request.setWorkTimes(editRequests);
 		return request;
+	}
+
+	private WorkTimeEditRequest toWorkTimeEditRequest(WorkTimeEditForm workTime) {
+		WorkTimeEditRequest editRequest = new WorkTimeEditRequest();
+		editRequest.setDate(workTime.getDate());
+		editRequest.setWorkType(workTime.getWorkType());
+
+		List<WorkTime.WorkType> nonTimeWorkTypes = Arrays.asList(
+				LEGAL_VACATION, FULL_PAID_VACATION, ABSENCE, COMPENSATORY_VACATION, SPECIAL_VACATION
+		);
+		boolean hasTimeWorkType = !nonTimeWorkTypes.contains(workTime.getWorkType());
+
+		boolean hasStartTime = hasTimeWorkType && nonNull(workTime.getStartAtHour()) && nonNull(workTime.getStartAtMinute());
+		LocalTime startAt = hasStartTime ? LocalTime.of(workTime.getStartAtHour(), workTime.getEndAtMinute()) : null;
+		editRequest.setStartAt(startAt);
+		boolean hasEndTime = hasTimeWorkType && nonNull(workTime.getEndAtHour()) && nonNull(workTime.getEndAtMinute());
+		LocalTime endAt = hasEndTime ? LocalTime.of(workTime.getEndAtHour(), workTime.getEndAtMinute()) : null;
+		editRequest.setEndAt(endAt);
+
+		Integer restTime = hasTimeWorkType ? workTime.getRestTime() : null;
+		editRequest.setRestTime(restTime);
+		editRequest.setRemarks(workTime.getRemarks());
+		return editRequest;
 	}
 }
