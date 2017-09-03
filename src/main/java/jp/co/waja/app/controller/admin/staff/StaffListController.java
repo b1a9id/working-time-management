61cb300be0a95164fa6790e77138aff5ba040e30
Ryosuke Uchitate <ry0suke.uch1tate@gmail.com>
2017-09-03 11:20:17 +0900

    スタッフ検索

--- src/main/java/jp/co/waja/app/controller/admin/staff/StaffListController.java
+++ src/main/java/jp/co/waja/app/controller/admin/staff/StaffListController.java
@@ -6,19 +6,29 @@ import jp.co.waja.core.service.worktime.WorkTimeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.*;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/admin/staffs")
 public class StaffListController {
+
+	private static final String FORM_MODEL_KEY = "form";
 
 	@Autowired
 	private StaffService staffService;
 
 	@Autowired
 	private WorkTimeService workTimeService;
+
+	@ModelAttribute("form")
+
 
 	@GetMapping
 	public String list(Model model) {
