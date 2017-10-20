cea13fb5fa56e4356da7d85e1fae67174e84cc3f
Ryosuke Uchitate <u.ryosuke@waja.jp>
2017-10-20 17:57:27 +0900

    集計結果をスライドで表示

--- src/main/resources/static/js/common/common.js
+++ src/main/resources/static/js/common/common.js
@@ -1,3 +1,11 @@
+$(document).ready(function () {
+	$('#accordion-btn').on('click', function () {
+		$('#accordion-block').stop(true, true).slideToggle();
+	});
+});
+
 $(function () {
 	$('[data-toggle="tooltip"]').tooltip()
-})
+});
+
+
