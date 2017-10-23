705a5bf90c4715c9c7582d1a3d9e08a3d079fd1e
Ryosuke Uchitate <u.ryosuke@waja.jp>
2017-10-23 16:53:16 +0900

    長期休暇登録

---
+++ src/main/java/jp/co/waja/core/model/staff/LongLeaveRequest.java
@@ -0,0 +1,22 @@
+package jp.co.waja.core.model.staff;
+
+import jp.co.waja.core.entity.LongLeave;
+import lombok.*;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class LongLeaveRequest implements Serializable {
+
+	private LongLeave.Type type;
+
+	private LocalDate startAt;
+
+	private LocalDate endAt;
+
+	private String remarks;
+}
