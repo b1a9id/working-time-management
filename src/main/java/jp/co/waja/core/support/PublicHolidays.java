2815f86a319adc74581e1157cc2a58558ae81b61
Ryosuke Uchitate <ry0suke.uch1tate@gmail.com>
2017-12-07 00:04:37 +0900

    修正

--- src/main/java/jp/co/waja/core/support/PublicHolidays.java
+++ src/main/java/jp/co/waja/core/support/PublicHolidays.java
@@ -7,25 +7,24 @@ import java.util.List;
 public class PublicHolidays {
 
 	public static List<LocalDate> getPublicHolidays() {
-		int thisYear = LocalDate.now().getYear();
 		return Arrays.asList(
-				LocalDate.of(thisYear, 1, 1),
-				LocalDate.of(thisYear, 1, 2),
-				LocalDate.of(thisYear, 1, 9),
-				LocalDate.of(thisYear, 2, 11),
-				LocalDate.of(thisYear, 3, 20),
-				LocalDate.of(thisYear, 4, 29),
-				LocalDate.of(thisYear, 5, 3),
-				LocalDate.of(thisYear, 5, 4),
-				LocalDate.of(thisYear, 5, 5),
-				LocalDate.of(thisYear, 7, 17),
-				LocalDate.of(thisYear, 8, 11),
-				LocalDate.of(thisYear, 9, 18),
-				LocalDate.of(thisYear, 9, 23),
-				LocalDate.of(thisYear, 10, 9),
-				LocalDate.of(thisYear, 11, 3),
-				LocalDate.of(thisYear, 11, 23),
-				LocalDate.of(thisYear, 12, 23)
+				LocalDate.of(2017, 1, 1),
+				LocalDate.of(2017, 1, 2),
+				LocalDate.of(2017, 1, 9),
+				LocalDate.of(2017, 2, 11),
+				LocalDate.of(2017, 3, 20),
+				LocalDate.of(2017, 4, 29),
+				LocalDate.of(2017, 5, 3),
+				LocalDate.of(2017, 5, 4),
+				LocalDate.of(2017, 5, 5),
+				LocalDate.of(2017, 7, 17),
+				LocalDate.of(2017, 8, 11),
+				LocalDate.of(2017, 9, 18),
+				LocalDate.of(2017, 9, 23),
+				LocalDate.of(2017, 10, 9),
+				LocalDate.of(2017, 11, 3),
+				LocalDate.of(2017, 11, 23),
+				LocalDate.of(2017, 12, 23)
 		);
 	}
 }
